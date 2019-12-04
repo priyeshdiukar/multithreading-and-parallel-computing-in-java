d43d3bd7cb4d6b2e5a08992ddfcacc3db75d5301
priyesh.diukar <priyesh.diukar@siemens.com>
2019-12-04 16:39:47 +0530

    Update semphore and Executor Service

---
+++ src/concurrency/ExecutorApp.java
@@ -0,0 +1,51 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class ExecutorWorker implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
+
+/*
+ * ExecutorService executorService = Executors.newCachedThreadPool(); - Going to
+ * return an executorservice that can dynamically reuse threads - Before
+ * starting a job —> it going to check whether there finished the job... if yes,
+ * reuse them. - If there are no waiting threads —> it is going to create
+ * another one - Good for the processor... effective solution.
+ * 
+ * ExecutorService executorService = Executors.newFixedThreadPool(N); - maximize
+ * the number of threads. - If we want to start a job —> if all the threads are
+ * busy, we have to wait for one to terminate
+ * 
+ * ExecutorService executorService = Executors.newSingIeThreadExecutor() ; - It
+ * uses a single thread for the job. execute () -> runnable + callable submit()
+ * -> runnable
+ */
+public class ExecutorApp {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        /* No multitasking */
+        //ExecutorService executorService = Executors.newSingleThreadExecutor();
+        /* Create as many threads as needed */
+        //ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+           executorService.submit(new ExecutorWorker());
+        }
+    }
+
+}
---
+++ src/concurrency/SemphoreApp.java
@@ -0,0 +1,55 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/* 
+- Semaphore maintains a set of permits
+- Acquire () ->  if a permit is available then takes it.
+- Release () -> adds a permit.
+Semaphore just keeps a count of the number available
+new Semaphore (int permits, Boolean fair)
+ */
+
+enum Downloader {
+    INSTANCE;
+
+    private Semaphore semaphore = new Semaphore(3, true);
+
+    public void downloadData() {
+        try {
+            semaphore.acquire();
+            download();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void download() {
+        System.out.println("Downloading data from the web...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class SemphoreApp {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 12; i++) {
+            executorService.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    Downloader.INSTANCE.downloadData();
+                }
+            });
+        }
+
+    }
+}
